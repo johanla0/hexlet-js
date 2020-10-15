5f0b6ade60da124ab9f7879af8931cc15cb5e134
johanla0 <idlhero@gmail.com>
2020-10-15 23:29:00 +0300

    formatted with eslint requirements

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import _ from "lodash";
+import _ from 'lodash';
 
-console.log("node and npm successfully installed, npm initialized");
-console.log(_.last(["one", "two"]));
+console.log('node and npm successfully installed, npm initialized');
+console.log(_.last(['one', 'two']));
